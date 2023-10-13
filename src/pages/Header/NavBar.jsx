f7afec85326e70fe1609d0aae8e643f9283c57d8
1Masab <133378401+1Masab@users.noreply.github.com>
2023-10-13 23:49:12 +0300

    Additional style modification to the nav Links

--- src/pages/Header/NavBar.jsx
+++ src/pages/Header/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLinks } from "../../data/Constants";
 import { useLanguage } from "../../context/LanguageContext";
-import { Link,useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const NavBar = () => {
   const { currentLanguage } = useLanguage(); // to access the translation
   const location = useLocation(); // Geting the current location
@@ -13,7 +13,9 @@ const NavBar = () => {
           <Link
             to={`${link.linkPath}`}
             className={`hover:text-[#35997b] rounded-lg  cursor-pointer transition duration-100 ${
-              location.pathname === link.linkPath ? 'border-b-2 border-primary rounded-none' : ''
+              location.pathname === link.linkPath
+                ? "border-b-2 border-primary text-[#35997b] rounded-none"
+                : ""
             }`}
           >
             {currentLanguage[link.linkName]}
